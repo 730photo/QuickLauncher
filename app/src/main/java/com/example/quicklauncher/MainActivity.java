b06ee08d001ed4f77cac5f268c46200303381d73
architekespy <architekespy@Reginalds-MacBook-Pro.local>
2019-05-02 15:42:37 -0400

    Initial commit

---
+++ app/src/main/java/com/example/quicklauncher/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.quicklauncher;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Attempts to launch an activity within our own app
+        Button secondActivityBtn = (Button)findViewById(R.id.secondActivityBtn);
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                startIntent.putExtra("com.example.quicklauncher.SOMETHING", "Hello World!");
+                startActivity(startIntent);
+            }
+        });
+
+        // Attempt to launch an activity outside our app
+        Button googleBtn = (Button)findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String google = "https://www.google.com";
+                Uri webaddress = Uri.parse(google);
+
+                Intent gotoGoogle = new Intent(Intent.ACTION_VIEW, webaddress);
+                if (gotoGoogle.resolveActivity(getPackageManager()) != null) {
+                    startActivity(gotoGoogle);
+                }
+
+            }
+        });
+
+    }
+}
